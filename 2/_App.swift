34d2ab1eeb46bfc1fb490e39c114f871a0cef439
efthimios <epadouvakis@gmail.com>
2025-03-22 15:47:19 +0200

    Initial Commit

---
+++ 2/_App.swift
@@ -0,0 +1,17 @@
+//
+//  _App.swift
+//  2
+//
+//  Created by Ευθύμης Παντουβάκης on 22/3/25.
+//
+
+import SwiftUI
+
+@main
+struct _App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 2Tests/_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  _Tests.swift
+//  2Tests
+//
+//  Created by Ευθύμης Παντουβάκης on 22/3/25.
+//
+
+import Testing
+@testable import _
+
+struct _Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
